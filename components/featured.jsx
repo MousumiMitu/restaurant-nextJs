d271c9a2f5f8819de3e64e69adc0ba85c70483b1
MousumiMitu <mousumitu@gmail.com>
2022-05-20 23:45:01 +0600

    food restaurant

---
+++ components/featured.jsx
@@ -0,0 +1,8 @@
+import styles from "../styles/Featured.module.css";
+import Image from "next/image";
+
+const Featured = () => {
+  return <div className={styles.container}>featured</div>;
+};
+
+export default Featured;
