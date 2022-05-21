d19a0f61dfa676c51c25a4d70468941f5b525774
MousumiMitu <mousumitu@gmail.com>
2022-05-21 22:51:29 +0600

    UI design

--- components/featured.jsx
+++ components/featured.jsx
@@ -2,7 +2,39 @@ import styles from "../styles/Featured.module.css";
 import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>featured</div>;
+  const images = [
+    "/images/slider1.jpg",
+    "/images/slider2.jpg",
+    "/images/slider3.jpg",
+  ];
+  return (
+    <div className={styles.container}>
+      <div className={styles.arrowContainer} style={{ left: 0 }}>
+        <Image
+          src="/images/arrowl.png"
+          alt=""
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+
+      <div className={styles.wrapper}>
+        {images.map((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt="" layout="fill" objectFit="contain" />
+          </div>
+        ))}
+      </div>
+      <div className={styles.arrowContainer} style={{ right: 0 }}>
+        <Image
+          src="/images/arrowr.png"
+          layout="fill"
+          alt=""
+          objectFit="contain"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
